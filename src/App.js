697ae4cbb0dbe4ef0d6171be1ae3d6584ceeef8e
Lokindra Dangi <pachar.com@gmail.com>
2021-01-26 22:48:18 +0545

    Basic Skeletopn Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.scss";
 import Header from "./Components/Header";
+import SocialMedia from "./Components/SocialMedia";
 import Banner from "./Pages/Banner";
+import Carbon from "./Pages/Carbon";
 import Card from "./Pages/Card";
 import FeaturesCard from "./Pages/FeaturesCard";
 import Xbox from "./Pages/Xbox";
@@ -13,6 +15,8 @@ function App() {
       <Card />
       <Xbox />
       <FeaturesCard />
+      <Carbon />
+      <SocialMedia />
     </div>
   );
 }
---
+++ src/Components/SocialMedia.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./SocialMedia.scss";
+
+function SocialMedia() {
+  return (
+    <section className="follow">
+      <p>Follow MicroSoft</p>
+      <a href="">
+        <img src="https://i.ibb.co/LrVMXNR/social-fb.png" alt="" />
+      </a>
+      <a href="">
+        <img src="https://i.ibb.co/vJvbLwm/social-twitter.png" alt="" />
+      </a>
+      <a href="">
+        <img src="https://i.ibb.co/b30HMhR/social-linkedin.png" alt="" />
+      </a>
+    </section>
+  );
+}
+
+export default SocialMedia;
